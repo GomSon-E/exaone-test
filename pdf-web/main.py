db2cea4750638ea1436d21ecdeb0bc091d9a43d6
GomSon-E <gomsonixx@gmail.com>
2025-04-24 09:06:52 +0000

    FIX : 포트 번호 수정

--- pdf-web/main.py
+++ pdf-web/main.py
@@ -83,4 +83,4 @@ async def get_thumbnail(filename: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5001, reload=True)
